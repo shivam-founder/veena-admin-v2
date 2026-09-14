b0f90a497526713a778fa0acec793831eab05c83
Shivam Nimkar <shivamnimkar145@gmail.com>
2026-09-14 19:42:57 +0530

    Implement logout handler to clear admin session

---
+++ api/logout.js
@@ -0,0 +1,4 @@
+export default async function handler(req, res) {
+  res.setHeader("Set-Cookie", "admin_session=; HttpOnly; Path=/; Secure; Max-Age=0; SameSite=Lax");
+  return res.status(200).json({ ok: true });
+}
